43e1fe3d99f1685eb53a0998906d150360fd87bb
Silva Kirsimae <silva.kirsimae-cic.netherlands@ibm.com>
2020-02-16 11:41:50 +0100

    Add Movie and Results struct

---
+++ MoviesApp/Models/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by SilvaKirsimae on 25/11/2019.
+//  Copyright © 2019 Silva. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let results: [Results]
+}
+
+struct Results: Codable {
+    let title: String
+    let id: Int
+    let poster_path: String
+}
