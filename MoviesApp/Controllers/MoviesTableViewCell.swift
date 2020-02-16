7f92c056369183136b64fc72194493a579cd73e8
Silva Kirsimae <silva.kirsimae-cic.netherlands@ibm.com>
2020-02-16 11:41:07 +0100

    Add Movies tableview cell

---
+++ MoviesApp/Controllers/MoviesTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MoviesTableViewCell.swift
+//  MoviesApp
+//
+//  Created by SilvaKirsimae on 25/11/2019.
+//  Copyright © 2019 Silva. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        
+    }
+    
+}
